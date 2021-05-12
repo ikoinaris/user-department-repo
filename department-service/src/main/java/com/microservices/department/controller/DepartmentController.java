e7aeef521d6e6ecefe279c1c2cd2cd96fcad6b2a
Ioannis Koinaris <ikoinaris@mgm-edv.de>
2021-05-12 11:38:55 +0200

    save Department and findById methods created

--- department-service/src/main/java/com/microservices/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/microservices/department/controller/DepartmentController.java
@@ -1,12 +1,28 @@
 package com.microservices.department.controller;
 
+import com.microservices.department.entity.Department;
 import com.microservices.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
 
     @Autowired
     private DepartmentService service;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department){
+        log.info("Inside saveDepartment method of controller");
+        return service.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartment(@PathVariable  Long id) {
+        log.info("Inside findDepartment method of controller");
+        return service.getById(id);
+    }
 }
--- department-service/src/main/java/com/microservices/department/service/DepartmentService.java
+++ department-service/src/main/java/com/microservices/department/service/DepartmentService.java
@@ -1,12 +1,25 @@
 package com.microservices.department.service;
 
+import com.microservices.department.entity.Department;
 import com.microservices.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
 
     @Autowired
     private DepartmentRepository repository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside of saveDepartment method of service");
+        return repository.save(department);
+    }
+
+    public Department getById(Long id) {
+        log.info("Inside findDepartment method of service");
+        return repository.findDepartmentById(id);
+    }
 }
